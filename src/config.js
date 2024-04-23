50981b7bdf25837f38c8b3366ad6dc48fe3693b6
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-23 21:34:04 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/notesdb";
+export const PORT = process.env.PORT || 80;
+export const MONGODB_URI = `mongodb+srv://rahulvikhe:${process.env.MONGO_PASSWORD}@dev-cluster.idtyynl.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`;
