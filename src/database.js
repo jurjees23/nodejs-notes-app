2191fcdcbfbe0ada6a044c8d706398c01acab7af
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-23 21:50:56 +0530

    Set the strictQuery option to suppress the deprecation warning

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "./config.js";
 
+mongoose.set('strictQuery', false);
+
 try {
   const db = await mongoose.connect(MONGODB_URI);
   console.log("Connected to ", db.connection.name);
