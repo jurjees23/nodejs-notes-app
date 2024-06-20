41815c642693ecbc06921cc48815ec14b4163c2b
jurjees23 <s.jurjeesahamed555@gmail.com>
2024-06-20 16:47:12 +0400

    config changed

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 80;
-export const MONGODB_URI = `mongodb+srv://rahulvikhe:${process.env.MONGO_PASSWORD}@dev-cluster.idtyynl.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`;
+export const MONGODB_URI = `mongodb+srv://jurjees23:${process.env.MONGO_PASSWORD}@cluster0.ma4cm.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`;
+
